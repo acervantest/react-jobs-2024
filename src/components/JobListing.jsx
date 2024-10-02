c1dd87089a1c69ca7cbdfa46466737fd2e587d3a
Alejandro <acervantest83@gmail.com>
2024-10-01 22:06:07 -0700

    Toggle show full job description and added react icons

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+import { FaMapMarker } from 'react-icons/fa'
+
 const JobListing = ({job: { type, title, description, salary, location, id }}) => {
+
+    const [showFullDescription, setShowFullDescription] = useState(false)
+
+    if(!showFullDescription) {
+        description = description.substring(0, 90) + '...'
+    }
+
     return ( 
         <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
@@ -11,14 +21,21 @@ const JobListing = ({job: { type, title, description, salary, location, id }}) =
                     {description}
                 </div>
 
+                <button className="text-indigo-500 mb-5 hover:text-indigo-60"
+                    onClick={() => setShowFullDescription((prevState) => !prevState)}
+                >
+                    {setShowFullDescription ? 'Less':'More'}
+                </button>
                 <h3 className="text-indigo-500 mb-2">{salary} / Year</h3>
 
                 <div className="border border-gray-100 mb-5"></div>
 
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                 <div className="text-orange-700 mb-3">
-                    <i className="fa-solid fa-location-dot text-lg"></i>
-                    {location}
+                <FaMapMarker className="inline text-lg mb-1 mr-1"/>
+                {location}
+                
+                    
                 </div>
                 <a
                     href={`/job/${id}`}
